8cf2e1e68ab6a519d17150338aefd2ab073004f0
Binh Le <sonybinhle@gmail.com>
2018-02-28 22:15:11 +0100

    Using eslint

--- src/deepMap.js
+++ src/deepMap.js
@@ -1,64 +1,64 @@
 'use strict';
 
-function isObject(o) {
-    return o !== null && typeof o === 'object';
+function isObject (o) {
+  return o !== null && typeof o === 'object';
 }
 
-function isFunction(f) {
-    return !!(f && f.constructor && f.call && f.apply);
+function isFunction (f) {
+  return !!(f && f.constructor && f.call && f.apply);
 }
 
-function defaultMapValue(value) {
-    return value;
+function defaultMapValue (value) {
+  return value;
 }
 
-function defaultMapArray(array, resolve) {
-    return array.map(resolve);
+function defaultMapArray (array, resolve) {
+  return array.map(resolve);
 }
 
-function defaultMapObject(object, resolve) {
-    var mappedObject = {};
+function defaultMapObject (object, resolve) {
+  var mappedObject = {};
 
-    for (var key in object) {
-        if (object.hasOwnProperty(key)) {
-            mappedObject[key] = resolve(object[key]);
-        }
+  for (var key in object) {
+    if (object.hasOwnProperty(key)) {
+      mappedObject[key] = resolve(object[key]);
+    }
+  }
+
+  return mappedObject;
+}
+
+function deepMap (options) {
+  var mapFn, mapValue, mapArray, mapObject;
+
+  if (!isObject(options) && !isFunction(options)) throw new Error('Options must be a function or a object');
+
+  if (isFunction(options)) {
+    options.mapValue = options;
+  }
+
+  mapFn = options.mapFn;
+  mapValue = options.mapValue || defaultMapValue;
+  mapArray = options.mapArray || defaultMapArray;
+  mapObject = options.mapObject || defaultMapObject;
+
+  function resolve (value) {
+    if (mapFn) {
+      return mapFn(value, resolve);
     }
 
-    return mappedObject;
-}
-
-function deepMap(options) {
-    var mapFn, mapValue, mapArray, mapObject;
-
-    if (!isObject(options) && !isFunction(options)) throw new Error('Options must be a function or a object');
-
-    if (isFunction(options)) {
-        options.mapValue = options;
+    if (Array.isArray(value)) {
+      return mapArray(value, resolve);
     }
 
-    mapFn = options.mapFn;
-    mapValue = options.mapValue || defaultMapValue;
-    mapArray = options.mapArray || defaultMapArray;
-    mapObject = options.mapObject || defaultMapObject;
-
-    function resolve(value) {
-        if (mapFn) {
-            return mapFn(value, resolve);
-        }
-
-        if (Array.isArray(value)) {
-            return mapArray(value, resolve);
-        }
-
-        if (isObject(value)) {
-            return mapObject(value, resolve);
-        }
-
-        return mapValue(value, resolve);
+    if (isObject(value)) {
+      return mapObject(value, resolve);
     }
 
-    return resolve;
+    return mapValue(value, resolve);
+  }
+
+  return resolve;
 }
 
 deepMap.mapArray = defaultMapArray;
